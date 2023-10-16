50a3319e24b7b9cb3cd40f9137837b4672c4aa71
Debasish Mohajan <109045949+dev-aasishh@users.noreply.github.com>
2023-10-17 02:02:14 +0530

    added

---
+++ script.js
@@ -0,0 +1,34 @@
+const hour=document.getElementById('hr');
+const minute=document.getElementById('min');
+const second=document.getElementById('sec');
+const toggle=document.getElementById('toggle');
+const body=document.querySelector('body');
+const col = document.getElementsByTagName('div');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background='white';
+        body.style.color='black';
+        body.style.transition='2s';
+    }
+    else{
+        body.style.background='rgb(40, 40, 43)';
+        document.getElementById("col").style.backgroundColor= 'white';
+        document.getElementById("col").style.boxShadow= '15px 15px 30px black,-15px -15px 30px black';
+
+        document.getElementById("col2").style.boxShadow= 'inset 15px 15px 30px grey,inset -15px -15px 30px black';
+        body.style.color='white';
+        body.style.transition='2s';  
+    }
+});
+setInterval(() =>
+{
+    let date=new Date();
+    let hr=date.getHours()*30;
+    let min=date.getMinutes()*6;
+    let sec=date.getSeconds()*6;
+    hour.style.transform=`rotateZ(${(hr)+(min/12)}deg)`;
+    minute.style.transform=`rotateZ(${min}deg)`;
+    second.style.transform=`rotateZ(${sec}deg)`;
+});
